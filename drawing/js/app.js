862b1bae5235752d8bbdb0a4e1d5337b7896e549
Xander Taylor <xanderoid@gmail.com>
2014-04-26 14:32:27 -0700

    added all files from workspace of drawing app

---
+++ drawing/js/app.js
@@ -0,0 +1,94 @@
+//Problem: No user interaction causes no change to application
+//Solution: When user interacts cause changes appropritely
+var color = $(".selected").css("background-color");
+var context = $("canvas")[0].getContext("2d");
+var $canvas = $("canvas");
+var lastEvent;
+var mouseDown = false;
+//When clicking on control list items
+$(".controls").on("click", "li", function(){
+//Deselect siblling elements
+  $(this).siblings().removeClass("selected");
+  //Select clicked element
+  $(this).addClass("selected");
+  //cache current color  
+  color = $(this).css("background-color");
+});
+  
+//when new color is pressed
+$("revealColorSelect").click(function(){
+  //Show color select or hide the color select
+  changeColor();
+  $("colorSelect").toggle();
+});
+
+  //update the "new color" span
+function changeColor() {
+  var r = $("#red").val();
+  var g = $("#green").val();
+  var b = $("#blue").val();
+  $("#newColor").css("background-color", "rgb(" + r + ", " + g + ", " + b + ")");
+}
+
+
+
+//When color sliders change
+$("input[type=range]").change(changeColor);
+
+//When "add color" is pressed
+$("#addNewColor").click(function(){
+  //Append the color to the controls ul
+  var $newColor = $("<li></li>");
+  $newColor.css("background-color"), $("#newColor").css("background-color");
+  $(".controls ul").append($newColor);
+  //Select the new color
+  $newColor.click();
+
+});
+
+
+//On mouse events on the canvas
+$canvas.mousedown(function(e){
+  LasteEvent = e;
+  mouseDown = true;
+}).mousemove(function(e){
+  //Draw lines
+  if(mouseDown){
+    context.beginPath();
+    context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+    context.lineTo(e.offsetX, e.offsetY);
+    context.stroke();
+    lastEvent = e;
+  }
+}).mouseup(function(){
+  mouseDown = false;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
