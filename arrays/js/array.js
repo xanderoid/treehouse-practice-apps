9ba4c93e494d52ab3e3d9d081ead698e6ddaeb55
Xander Taylor <xanderoid@gmail.com>
2014-04-26 20:53:32 -0700

    added 'arrays' folder to contain js practice files

---
+++ arrays/js/array.js
@@ -0,0 +1,54 @@
+// JavaScript Arrays
+
+// var x =['some', 'words', 'here'];
+// //console.log(x.length);
+
+// var y =['a string', 3, ['a sub array', 2, 3, 4], "treehouse", function(a,b){return a+b;}];
+
+// var z = [];
+
+
+
+
+// Getting and Setting
+
+// var my_array = ['Hello', 42, true, function(a){return a*2}];
+
+// var word = my_array[0];
+
+// my_array[1] = 144
+// var new_answer = my_array[1];
+
+// my_array[my_array.length] = 'a new string'
+
+// var myArray = ["sugar", "rush", "fix", "it", 3.14, 42];
+// var thirdElementInArray = myArray[2];
+
+
+
+
+
+
+//Methods Part 1
+
+// var my_array = [2, 3, 4,];
+// console.log(my_array.toString());
+
+// my_array.push('texas');
+// console.log(my_array.toString());
+
+// var value = my_array.pop();
+
+
+
+
+
+
+var my_array = [2,3,4]
+console.log(my_array.toString());
+
+my_array.unshift(1);
+console.log(my_array.toString());
+
+
+
