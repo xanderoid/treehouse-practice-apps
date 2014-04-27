cbae3ead9a6254ddc897a27dfb75540f4f7b633f
Xander Taylor <xanderoid@gmail.com>
2014-04-27 08:27:48 -0700

    finished video 2

---
+++ objects/objects.js
@@ -0,0 +1,37 @@
+//Javascript Objects
+
+// var jim = { 
+// 	name: "Jim", 
+// 	skills: ["Javascript", "Ruby", "Dancing"],
+// 	"favorite color": "green" 
+// };
+
+// console.log(jim.name);
+// console.log(jim["favorite color"]);
+
+//video 2
+
+// var jim = { 
+// 	name: "Jim", 
+// 	skills: ["Javascript", "Ruby", "Dancing"],
+// 	"favorite color": "green", 
+// 	greet: function () {
+// 		console.log('Hello, I am ' + jim.name);
+// 	}
+// };
+
+// var nick = { 
+// 	name: "Nick", 
+// 	greet: function () {
+// 		console.log('Hello, I am ' + nick.name);
+// 	}
+// };
+
+// // console.log(jim.name);
+// // console.log(jim["favorite color"]);
+
+// // jim.name = "James";
+// jim.greet();
+
+//video 3
+
